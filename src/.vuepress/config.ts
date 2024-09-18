5442ca87ca14e670c2a23345db613b63ad34060b
Taoning Ge <gtn1024@foxmail.com>
2024-09-18 14:01:09 +0800

    chore: init

---
+++ src/.vuepress/config.ts
@@ -0,0 +1,25 @@
+import { defineUserConfig } from "vuepress";
+
+import theme from "./theme.js";
+
+export default defineUserConfig({
+  base: "/",
+
+  locales: {
+    "/": {
+      lang: "en-US",
+      title: "Cangjie by Example",
+      description: "Learn cangjie programming language by example",
+    },
+    "/zh/": {
+      lang: "zh-CN",
+      title: "Cangjie by Example",
+      description: "通过例子学习仓颉编程语言",
+    },
+  },
+
+  theme,
+
+  // Enable it with pwa
+  // shouldPrefetch: false,
+});
---
+++ src/.vuepress/theme.ts
@@ -0,0 +1,52 @@
+import { hopeTheme } from "vuepress-theme-hope";
+
+import { enNavbar, zhNavbar } from "./navbar/index.js";
+import { enSidebar, zhSidebar } from "./sidebar/index.js";
+
+export default hopeTheme({
+  hostname: "https://cangjiebyexample.com",
+
+  repo: "gtn1024/cangjiebyexample",
+
+  docsDir: "src",
+
+  locales: {
+    "/": {
+      // navbar
+      navbar: enNavbar,
+
+      // sidebar
+      sidebar: enSidebar,
+
+      footer: "Default footer",
+
+      displayFooter: false,
+
+      metaLocales: {
+        editLink: "Edit this page on GitHub",
+      },
+    },
+
+    "/zh/": {
+      // navbar
+      navbar: zhNavbar,
+
+      // sidebar
+      sidebar: zhSidebar,
+
+      footer: "默认页脚",
+
+      displayFooter: false,
+
+      metaLocales: {
+        editLink: "在 GitHub 上编辑此页",
+      },
+    },
+  },
+
+  plugins: {
+    components: {
+      components: ["Badge", "VPCard"],
+    },
+  },
+});
